b1d463dadb75c0b53d99715452a916fe76997c2f
Artyom Khaov <a.khaov@gmail.com>
2021-12-13 02:04:33 +0700

    Solved: task (7 kyu) List Filtering

---
+++ 7-kyu-list-filtering/script.js
@@ -0,0 +1,13 @@
+// Solution:
+
+function filter_list(l) {
+    return filteredList = l.filter(function (item) {
+        return typeof item === 'number';
+    });
+}
+
+// Sample Tests:
+
+filter_list([1,2,'a','b']) // [1,2]
+filter_list([1,'a','b',0,15]) // [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) // [1,2,123]
