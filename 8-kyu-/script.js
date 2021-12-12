d6b3034aaf8a3ba007138c6b88973d4676e9aa68
Artyom Khaov <a.khaov@gmail.com>
2021-12-13 02:52:44 +0700

    Solved: task (8 kyu) Counting sheep... / filter method

---
+++ 8-kyu-/script.js
@@ -0,0 +1,19 @@
+var array1 = [true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true ];
+
+// Solution:
+
+function countSheeps(arrayOfSheep) {
+  const numberOfSheep = arrayOfSheep.filter(function (item) {
+    return item;
+  });
+  return numberOfSheep.length;
+}
+
+// Sample Tests:
+
+countSheeps(array1) // 17
