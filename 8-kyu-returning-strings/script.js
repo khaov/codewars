eaef5b64af3f9715014f71047cbb68d5891c0df4
Artyom Khaov <a.khaov@gmail.com>
2022-01-04 00:23:55 +0700

    Solved: task (8 kyu) Returning Strings

---
+++ 8-kyu-returning-strings/script.js
@@ -0,0 +1,10 @@
+// Solution:
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
+// Sample Tests:
+
+greet("Ryan")     // "Hello, Ryan how are you doing today?"
+greet("Shingles") // "Hello, Shingles how are you doing today?"
