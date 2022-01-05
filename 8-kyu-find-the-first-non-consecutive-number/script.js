2d13917e0ed190ae4039108eaf042a304b8a9c3b
Artyom Khaov <a.khaov@gmail.com>
2022-01-05 14:43:43 +0700

    Solved: task (8 kyu) Find the first non-consecutive number (for + if solution)

--- 8-kyu-find-the-first-non-consecutive-number/script.js
+++ 8-kyu-find-the-first-non-consecutive-number/script.js
@@ -1,14 +1,23 @@
 // Solution:
 
 function firstNonConsecutive (arr) {
-  const result = arr.find(function(number, index, array) {
-    return (index > 0 && Math.abs(number - array[index-1]) !== 1);
-  });
 
-  // С помощью метода Number.isInteger() определяем,
-  // является ли переданное значение result целым числом.
 
-  return Number.isInteger(result) ? result : null
+  // const result = arr.find(function(number, index, array) {
+  //   return (index > 0 && Math.abs(number - array[index-1]) !== 1);
+  // });
+
+  // // С помощью метода Number.isInteger() определяем,
+  // // является ли переданное значение result целым числом.
+
+  // return Number.isInteger(result) ? result : null
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (Math.abs(arr[i] - arr[i+1]) !== 1) {
+      return arr[i+1];
+    }
+  }
+  return null;
 }
 
 // Sample Tests:
