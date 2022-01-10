6579e2ea6d4b67654cb9871b819e2a7547b6aa85
Artyom Khaov <a.khaov@gmail.com>
2022-01-11 01:12:41 +0700

    Solved: task (8 kyu) Drink about (switch case solution)

--- 8-kyu-drink-about/script.js
+++ 8-kyu-drink-about/script.js
@@ -1,15 +1,30 @@
 // Solution:
 
 function peopleWithAgeDrink(old) {
-  if (old < 14) {
-    return 'drink toddy'
-  } else if (old < 18) {
-    return 'drink coke'
-  } else if (old < 21) {
-    return 'drink beer'
-  } else {
-    return 'drink whisky'
+  // if (old < 14) {
+  //   return 'drink toddy'
+  // } else if (old < 18) {
+  //   return 'drink coke'
+  // } else if (old < 21) {
+  //   return 'drink beer'
+  // } else {
+  //   return 'drink whisky'
+  // }
+
+  switch (true) {
+    case old < 14:
+      return 'drink toddy'
+      break;
+    case old < 18:
+      return 'drink coke'
+      break;
+    case old < 21:
+      return 'drink beer'
+      break;
+    default:
+      return 'drink whisky'
   }
+
 };
 
 // Sample Tests:
