957a7363040feb8b6c338ebae803b21e7ee17691
Artyom Khaov <a.khaov@gmail.com>
2022-01-11 00:58:07 +0700

    Solved: task (8 kyu) Drink about (if else solution)

---
+++ 8-kyu-drink-about/script.js
@@ -0,0 +1,23 @@
+// Solution:
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return 'drink toddy'
+  } else if (old < 18) {
+    return 'drink coke'
+  } else if (old < 21) {
+    return 'drink beer'
+  } else {
+    return 'drink whisky'
+  }
+};
+
+// Sample Tests:
+
+peopleWithAgeDrink(13) // 'drink toddy'
+peopleWithAgeDrink(17) // 'drink coke'
+peopleWithAgeDrink(18) // 'drink beer'
+peopleWithAgeDrink(20) // 'drink beer'
+peopleWithAgeDrink(21) // 'drink whisky'
+peopleWithAgeDrink(22) // 'drink whisky'
+peopleWithAgeDrink(30) // 'drink whisky'
