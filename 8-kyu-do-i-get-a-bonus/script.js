cebc747125feeb02c232cb6d8ee8cb6da8e91a4d
Artyom Khaov <a.khaov@gmail.com>
2022-01-20 21:07:38 +0700

    Solved: task (8 kyu) Do I get a bonus?

---
+++ 8-kyu-do-i-get-a-bonus/script.js
@@ -0,0 +1,19 @@
+// Solution:
+
+function bonusTime(salary, bonus) {
+  if (bonus){
+    return `\u00A3${salary*10}`
+  } else {
+    return `\u00A3${salary}`
+  }
+}
+
+// Sample Tests:
+
+bonusTime(10000, true)  // '£100000'
+bonusTime(25000, true)  // '£250000'
+bonusTime(10000, false) // '£10000'
+bonusTime(60000, false) // '£60000'
+bonusTime(2, true)      // '£20'
+bonusTime(78, false)    // '£78'
+bonusTime(67890, true)  // '£678900'
