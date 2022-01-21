526f1da957454e9a2881724ae633a0cef2d985d2
Artyom Khaov <a.khaov@gmail.com>
2022-01-21 20:44:53 +0700

    Solved: task (5 kyu) Where my anagrams at? (Case sensitive tests)

--- 5-kyu-where-my-anagrams-at/test.js
+++ 5-kyu-where-my-anagrams-at/test.js
@@ -1,6 +1,7 @@
-describe("Tests", function() {
-  it("test", function() {
+describe("Anagrams", function() {
+  it("anagram", function() {
     assert.deepEqual(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa']);
+    assert.deepEqual(anagrams('abba', ['AABB', 'abcd', 'BBAA', 'dada']), ['AABB', 'BBAA']);
     assert.deepEqual(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'])
     assert.deepEqual(anagrams('laser', ['lazing', 'lazy',  'lacer']), [])
   });
