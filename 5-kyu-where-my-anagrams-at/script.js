a9e3c731e87a28184607c8da2cc583d4ccb32d59
Artyom Khaov <a.khaov@gmail.com>
2022-01-21 20:35:59 +0700

    Solved: task (5 kyu) Where my anagrams at? (Case sensitive solution)

--- 5-kyu-where-my-anagrams-at/script.js
+++ 5-kyu-where-my-anagrams-at/script.js
@@ -2,7 +2,7 @@
 
 function anagrams(word, words) {
   function anagram(item) {
-    return item.split('').sort().join('');
+    return item.split('').sort().join('').toLowerCase();
   };
 
   return words.filter(function(element){
@@ -13,5 +13,6 @@ function anagrams(word, words) {
 // Sample Tests:
 
 anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])                // ['aabb', 'bbaa']
+anagrams('abba', ['AABB', 'abcd', 'BBAA', 'dada'])                // ['aabb', 'bbaa']
 anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) // ['carer', 'racer']
 anagrams('laser', ['lazing', 'lazy',  'lacer'])                   // []
