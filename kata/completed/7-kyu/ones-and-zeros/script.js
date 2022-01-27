2e1667b28e408793c91ce0e6b9f13f547f775d6c
Artyom Khaov <a.khaov@gmail.com>
2022-01-28 01:47:47 +0700

    Solved: completed kata (7 kyu) Ones and Zeros

---
+++ kata/completed/7-kyu/ones-and-zeros/script.js
@@ -0,0 +1,12 @@
+// Solution:
+
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(''), 2)
+};
+
+// Sample Tests:
+
+binaryArrayToNumber([0,0,0,1]) // 1
+binaryArrayToNumber([0,0,1,0]) // 2
+binaryArrayToNumber([1,1,1,1]) // 15
+binaryArrayToNumber([0,1,1,0]) // 6
