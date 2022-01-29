978b00a9f4677c819d3f8f4f22afca948bc4d1ec
Artyom Khaov <a.khaov@gmail.com>
2022-01-30 00:12:08 +0700

    Solved: completed kata (8 kyu) Convert a string to an array

---
+++ kata/completed/8-kyu/convert-a-string-to-an-array/script.js
@@ -0,0 +1,10 @@
+// Solution:
+
+function stringToArray(string){
+  return string.split(' ')
+}
+
+// Sample Tests:
+
+stringToArray("Robin Singh") // ["Robin", "Singh"]);
+stringToArray("I love arrays they are my favorite") // ["I", "love", "arrays", "they", "are", "my", "favorite"]);
