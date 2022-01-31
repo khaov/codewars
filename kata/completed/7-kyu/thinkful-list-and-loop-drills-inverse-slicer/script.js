1181d883a9680b8dbfc8fe0b424152a0dc8fde43
Artyom Khaov <a.khaov@gmail.com>
2022-02-01 00:21:13 +0700

    Solved: completed kata (7 kyu) Thinkful - List and Loop Drills: Inverse Slicer (push solution)

---
+++ kata/completed/7-kyu/thinkful-list-and-loop-drills-inverse-slicer/script.js
@@ -0,0 +1,17 @@
+// Solution:
+
+function inverseSlice(items, a, b) {
+
+  const leftSide = items.slice(0, a);
+  const reightSide = items.slice(b, items.length);
+
+  reightSide.forEach((number) => leftSide.push(number));
+
+  return leftSide
+}
+
+// Sample Tests:
+
+inverseSlice([12, 14, 63, 72, 55, 24], 2, 4) // [12, 14, 55, 24]
+inverseSlice([12, 14, 63, 72, 55, 24], 0, 3) // [72, 55, 24]
+inverseSlice(['Intuition', 'is', 'a', 'poor', 'guide', 'when', 'facing', 'probabilistic', 'evidence'], 5, 13) // ['Intuition', 'is', 'a', 'poor', 'guide']
