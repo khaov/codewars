bec65e3a4f80f81342d52d849a5b33364f531bcc
Artyom Khaov <a.khaov@gmail.com>
2022-02-03 00:31:35 +0700

    Added: unfinished kata (5 kyu) and (6 kyu) folders

---
+++ kata/unfinished/6-kyu/repeated-substring/script.js
@@ -0,0 +1,13 @@
+// Solution:
+
+function f(s) {
+  for (let i = 0; i <= s.length-1; i++) {
+
+  }
+}
+
+// Sample Tests:
+
+f("ababab") // ["ab", 3]
+//f("abcde")  // ["abcde", 1]
+//f("abcba")  // ["abcba", 1 ]
