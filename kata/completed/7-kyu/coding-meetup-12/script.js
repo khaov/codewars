c8d75323ffc2bae3b53a0f0f3be4045bfafd8853
Artyom Khaov <a.khaov@gmail.com>
2022-02-05 01:00:42 +0700

    Solved: completed kata (7 kyu) Coding Meetup #12 - Higher-Order Functions Series - Find GitHub admins

---
+++ kata/completed/7-kyu/coding-meetup-12/script.js
@@ -0,0 +1,23 @@
+// Solution:
+
+function findAdmin(list, lang) {
+  return list.filter(key => key.language === lang &&  key.githubAdmin === 'yes')
+}
+
+// Sample Tests:
+
+var list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+];
+
+var answer1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' }
+];
+
+findAdmin(list1, 'JavaScript')  // answer1
+findAdmin(list1, 'Ruby')        // []
+findAdmin(list1, 'Python')      // []
