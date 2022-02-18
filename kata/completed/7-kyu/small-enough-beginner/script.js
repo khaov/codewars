c7385432d03b2b7775f0a7696a781f6533852152
Artyom Khaov <a.khaov@gmail.com>
2022-02-18 20:48:12 +0700

    Solved: completed kata (7 kyu) Small enough? - Beginner

---
+++ kata/completed/7-kyu/small-enough-beginner/script.js
@@ -0,0 +1,12 @@
+// Solution:
+
+function smallEnough(a, limit){
+  return  a.every(item => item <= limit)
+}
+
+// Sample Tests:
+
+smallEnough([66, 101], 200) // true
+smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100) // false
+smallEnough([101, 45, 75, 105, 99, 107], 107) // true
+smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120) // true
