0eaae26e58837048ba27fc92f83105730b05a55b
Artyom Khaov <a.khaov@gmail.com>
2022-02-18 20:38:10 +0700

    Solved: completed kata (8 kyu) Third Angle of a Triangle

---
+++ kata/completed/8-kyu/third-angle-of-a-triangle/script.js
@@ -0,0 +1,12 @@
+// Solution:
+
+function otherAngle(a, b) {
+  return 180 - a - b;
+}
+
+// Sample Tests:
+
+otherAngle(30, 60) // 90
+otherAngle(60, 60) // 60
+otherAngle(43, 78) // 59
+otherAngle(10, 20) // 150
