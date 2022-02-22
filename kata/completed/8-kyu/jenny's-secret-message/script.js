d4aee543b813c08bc6934f683721d45cb80c72bb
Artyom Khaov <a.khaov@gmail.com>
2022-02-22 22:19:10 +0700

    Solved: completed kata (8 kyu) Jenny's secret message

---
+++ kata/completed/8-kyu/jenny's-secret-message/script.js
@@ -0,0 +1,12 @@
+// Solution:
+
+function greet(name){
+  return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
+
+// Sample Tests:
+
+greet("Jim")    // "Hello, Jim!"
+greet("Jane")   // "Hello, Jane!"
+greet("Simon")  // "Hello, Simon!"
+greet("Johnny") // "Hello, my love!"
