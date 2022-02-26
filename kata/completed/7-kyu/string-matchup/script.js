52c262b4ca7c25b041a7484b71a85ddbb155c5c6
Artyom Khaov <a.khaov@gmail.com>
2022-02-26 16:27:12 +0700

    Solved: completed kata (7 kyu) String matchup

---
+++ kata/completed/7-kyu/string-matchup/script.js
@@ -0,0 +1,29 @@
+// Solution:
+
+function solve(a,b){
+  let result = [];
+
+  b.forEach(function(secondArrayElement){
+    if (a.includes(secondArrayElement)) {
+      let counter = 0;
+
+      a.forEach(function(firstArrayElement){
+        if (firstArrayElement === secondArrayElement) {
+          counter ++
+        }
+      });
+
+      result.push(counter);
+    } else {
+      result.push(0);
+    }
+  });
+
+  return result
+}
+
+// Sample Tests:
+
+solve(['abc', 'abc','xyz','abcd','cde'], ['abc', 'cde', 'uap'])           // [2, 1, 0]
+solve(['abc', 'xyz','abc', 'xyz','cde'], ['abc', 'cde', 'xyz'])           // [2, 1, 2]
+solve(['quick', 'brown', 'fox', 'is', 'quick'], ['quick', 'abc', 'fox'])  // [2, 0, 1]
