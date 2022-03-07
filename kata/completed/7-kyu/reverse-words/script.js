b67ab5fbacff22e1314af8cbec18e4a2e22e0848
Artyom Khaov <a.khaov@gmail.com>
2022-03-07 14:13:43 +0700

    Solved: completed kata (7 kyu) Reverse words

---
+++ kata/completed/7-kyu/reverse-words/script.js
@@ -0,0 +1,12 @@
+// Solution:
+
+function reverseWords(str) {
+  return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
+}
+
+// Sample Tests:
+
+reverseWords('The quick brown fox jumps over the lazy dog.')  // 'ehT kciuq nworb xof spmuj revo eht yzal .god'
+reverseWords('apple')                                         // 'elppa'
+reverseWords('a b c d')                                       // 'a b c d'
+reverseWords('double  spaced  words')                         // 'elbuod  decaps  sdrow'
