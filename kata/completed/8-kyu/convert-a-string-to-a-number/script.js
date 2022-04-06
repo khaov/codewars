f22f36ac0dc71836a4239e40cde5fe748ccd0901
Artyom Khaov <a.khaov@gmail.com>
2022-04-06 15:54:55 +0700

    Solved: completed kata (8 kyu) Convert a String to a Number

---
+++ kata/completed/8-kyu/convert-a-string-to-a-number/script.js
@@ -0,0 +1,12 @@
+// Solution:
+
+var stringToNumber = function(str){
+  return Number(str);
+}
+
+// Sample Tests:
+
+stringToNumber("1234")  //  1234
+stringToNumber("605")   //  605
+stringToNumber("1405")  //  1405
+stringToNumber("-7")    //  -7
