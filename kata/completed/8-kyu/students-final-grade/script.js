6a1a49c14351b9fa88f8ca007a7aeaf89256bc57
Artyom Khaov <a.khaov@gmail.com>
2022-04-06 15:24:25 +0700

    Solved: completed kata (8 kyu) Student's Final Grade

---
+++ kata/completed/8-kyu/students-final-grade/script.js
@@ -0,0 +1,19 @@
+// Solution:
+
+function finalGrade (exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100
+  } else if (exam > 75 && projects >= 5) {
+    return 90
+  } else if (exam > 50 && projects >= 2) {
+    return 75
+  } else {
+    return 0
+  }
+}
+
+// Sample Tests:
+
+finalGrade(100, 12) // 100
+finalGrade(85, 5)   // 90
+finalGrade(0, 11)   // 90
