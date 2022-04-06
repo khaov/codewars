33d1288e0b2f766481ae5a3a9750508c6818c413
Artyom Khaov <a.khaov@gmail.com>
2022-04-06 18:30:12 +0700

    Solved: completed kata (8 kyu) Keep up the hoop

---
+++ kata/completed/8-kyu/keep-up-the-hoop/script.js
@@ -0,0 +1,10 @@
+// Solution:
+
+function hoopCount (n) {
+  return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+}
+
+// Sample Tests:
+
+hoopCount(3)  //  "Keep at it until you get it"
+hoopCount(11) //  "Great, now move on to tricks"
