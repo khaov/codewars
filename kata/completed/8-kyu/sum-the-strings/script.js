71e09d2633c68078390faf44b73f2ec7c120223b
Artyom Khaov <a.khaov@gmail.com>
2022-04-07 15:30:57 +0700

    Solved: completed kata (8 kyu) Sum The Strings

---
+++ kata/completed/8-kyu/sum-the-strings/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+function sumStr(a,b) {
+  return (Number(a) + Number(b)).toString()
+}
+
+// Sample Tests:
+
+sumStr("","")     // "0"
+sumStr("4","5")   // "9"
+sumStr("34","5")  // "39"
