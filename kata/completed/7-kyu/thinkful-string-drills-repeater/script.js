f45ad10a9c3512f178f44e311e18a40e321785ee
Artyom Khaov <a.khaov@gmail.com>
2022-04-07 12:42:18 +0700

    Solved: completed kata (7 kyu) Thinkful - String Drills: Repeater

---
+++ kata/completed/7-kyu/thinkful-string-drills-repeater/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+function repeater(string, n){
+  return string.repeat(n);
+}
+
+// Sample Tests:
+
+repeater('a', 5)    //  'aaaaa'
+repeater('Na', 16)  //  'NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa'
+repeater('Wub ', 6) //  'Wub Wub Wub Wub Wub Wub '
