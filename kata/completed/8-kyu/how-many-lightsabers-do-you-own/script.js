1eec143d89aa35b3a7a8d61331708ddfe963e3bb
Artyom Khaov <a.khaov@gmail.com>
2022-04-11 20:15:39 +0700

    Solved: completed kata (8 kyu) How many lightsabers do you own?

---
+++ kata/completed/8-kyu/how-many-lightsabers-do-you-own/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+function howManyLightsabersDoYouOwn(name) {
+  return name === "Zach" ? 18 : 0;
+}
+
+// Sample Tests:
+
+howManyLightsabersDoYouOwn()        // 0
+howManyLightsabersDoYouOwn("Adam")  // 0
+howManyLightsabersDoYouOwn("Zach")  // 18
