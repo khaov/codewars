601ee0bead139fecb0407fd1d094948ab8705061
Artyom Khaov <a.khaov@gmail.com>
2022-04-22 22:37:23 +0700

    Solved: completed kata (8 kyu) Get Nth Even Number

---
+++ kata/completed/8-kyu/Get Nth Even Number/script.js
@@ -0,0 +1,14 @@
+// Solution:
+
+function nthEven(n){
+  return n * 2 - 2
+}
+
+// Sample Tests:
+
+nthEven(1)        // 0, "Wrong Value!"
+nthEven(2)        // 2, "Wrong Value!"
+nthEven(3)        // 4, "Wrong Value!"
+
+nthEven(100)      // 198, "Wrong Value!"
+nthEven(1298734)  // 2597466, "Wrong Value!"
