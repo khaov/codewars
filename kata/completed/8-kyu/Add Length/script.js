2a3492cf01863f4f3ac20be3b01e75ec2281a273
Artyom Khaov <a.khaov@gmail.com>
2022-05-01 12:23:12 +0700

    Solved: completed kata (8 kyu) Add Length

---
+++ kata/completed/8-kyu/Add Length/script.js
@@ -0,0 +1,10 @@
+// Solution:
+
+function addLength(str) {
+  return str.split(' ').map(word => `${word} ${word.length}`)
+}
+
+// Sample Tests:
+
+addLength('apple ban')    // ["apple 5", "ban 3"]
+addLength('you will win') // ["you 3", "will 4", "win 3"]
