3a3dcbf7e7a5645f8bbffe033fb604018c3f0e3d
Artyom Khaov <a.khaov@gmail.com>
2022-05-19 19:09:37 +0700

    Added: unfinished kata (6 kyu) Reverse polish notation calculator

---
+++ kata/unfinished/6-kyu/Reverse polish notation calculator/script.js
@@ -0,0 +1,17 @@
+// Solution:
+
+function calc(expr) {
+  const expression = expr.split(' ');
+
+  
+}
+
+// Sample Tests:
+
+calc("")      // 0
+calc("3")     // 3
+calc("3.5")   // 3.5
+calc("1 3 +") // 4
+calc("1 3 *") // 3
+calc("1 3 -") // -2
+calc("4 2 /") // 2
