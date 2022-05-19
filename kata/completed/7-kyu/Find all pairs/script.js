f02c52b06bf7315522c21e88b1f79173e55ae98b
Artyom Khaov <a.khaov@gmail.com>
2022-05-19 20:49:12 +0700

    Solved: completed kata (7 kyu) Find all pairs (unique numbers solution)

---
+++ kata/completed/7-kyu/Find all pairs/script.js
@@ -0,0 +1,28 @@
+// Solution:
+
+function duplicates(array) {
+  let pairs = 0;
+
+  const duplicatedNumbers = array.filter((number, index, array) =>
+    !(index === array.lastIndexOf(number)) || !(index === array.indexOf(number))
+  );
+
+  const uniquedNumbers = duplicatedNumbers.filter((number, index, array) =>
+    index === array.lastIndexOf(number)
+  ).sort((prev, next) => prev - next);
+
+  uniquedNumbers.forEach(duplicate =>
+    pairs += Math.floor(duplicatedNumbers.filter(number => number === duplicate).length / 2)
+  );
+
+  return pairs;
+}
+
+// Sample Tests:
+
+duplicates([1, 2, 5, 6, 5, 2])            // 2
+duplicates([1, 2, 2, 20, 6, 20, 2, 6, 2]) // 4
+duplicates([0, 0, 0, 0, 0, 0, 0])         // 3
+duplicates([1000, 1000])                  // 1
+duplicates([])                            // 0
+duplicates([54])                          // 0
