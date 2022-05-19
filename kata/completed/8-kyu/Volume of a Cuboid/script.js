db243fdeebff15580a2f8a59c974c77f40fa4403
Artyom Khaov <a.khaov@gmail.com>
2022-05-19 19:23:08 +0700

    Solved: completed kata (8 kyu) Volume of a Cuboid

---
+++ kata/completed/8-kyu/Volume of a Cuboid/script.js
@@ -0,0 +1,12 @@
+// Solution:
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+// Sample Tests:
+
+Kata.getVolumeOfCuboid(1,2,2)   // 4
+Kata.getVolumeOfCuboid(6.3,2,5) // 63
