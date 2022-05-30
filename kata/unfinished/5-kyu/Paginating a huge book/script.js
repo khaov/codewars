afa5c096cada980f08f3c7254b33c2d663395d3d
Artyom Khaov <a.khaov@gmail.com>
2022-05-30 21:24:34 +0700

    Added: unfinished kata (5 kyu) Paginating a huge book

---
+++ kata/unfinished/5-kyu/Paginating a huge book/script.js
@@ -0,0 +1,21 @@
+// Solution:
+
+function pageDigits(pages) {
+  let length = pages.toString().length;
+
+  if (length === 1) {
+    return pages;
+  } else {
+    let digitsArray = [9];
+  
+    for (let number = 1; number <= length; number++) {
+
+    }
+  }
+}
+
+// Sample Tests:
+
+pageDigits(4n) // 4n
+pageDigits(12n) // 15n
+pageDigits(100n) // 192n
