62e550f5e8fe56629dee231ddd242851ead31605
Artyom Khaov <a.khaov@gmail.com>
2022-07-25 09:44:54 +0700

    Solved: completed kata (8 kyu) get character from ASCII Value

---
+++ kata/completed/8-kyu/get character from ASCII Value/script.js
@@ -0,0 +1,19 @@
+// Solution:
+
+function getChar(c){
+  return String.fromCharCode(c);
+}
+
+// Sample Tests:
+
+getChar(55) // '7'
+getChar(56) // '8'
+getChar(57) // '9'
+getChar(58) // ':'
+getChar(59) // ';'
+getChar(60) // '<'
+getChar(61) // '='
+getChar(62) // '>'
+getChar(63) // '?'
+getChar(64) // '@'
+getChar(65) // 'A'
