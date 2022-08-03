519f9289798c44105dabc32e0630a8a988928b6b
Artyom Khaov <a.khaov@gmail.com>
2022-08-03 11:08:27 +0700

    Solved: completed kata (8 kyu) Greet Me

---
+++ kata/completed/7-kyu/Greet Me/script.js
@@ -0,0 +1,10 @@
+// Solution:
+
+var greet = function(name) {
+  return 'Hello' + ` ${name.slice(0, 1).toUpperCase() +  name.slice(1).toLowerCase()}!`
+};
+
+// Sample Tests:
+
+greet('riley')  // 'Hello Riley!'
+greet('JACK')   // 'Hello Jack!'
