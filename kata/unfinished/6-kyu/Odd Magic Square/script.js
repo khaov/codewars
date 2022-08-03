d2559258249759adeb62a20da66ae107b06949d2
Artyom Khaov <a.khaov@gmail.com>
2022-08-03 09:50:39 +0700

    Added: unfinished kata (6 kyu) Odd Magic Square

---
+++ kata/unfinished/6-kyu/Odd Magic Square/script.js
@@ -0,0 +1,13 @@
+// Solution:
+
+function magicSquare(n) {
+  let square = new Array(n);
+
+  return square;
+}
+
+// Sample Tests:
+
+magicSquare(3) // [[8, 1, 6], [3, 5, 7], [4, 9, 2]]
+magicSquare(5) // [[17, 24, 1, 8, 15], [23, 5, 7, 14, 16], [4, 6, 13, 20, 22], [10, 12, 19, 21, 3], [11, 18, 25, 2, 9]]
+magicSquare(1) // [[1]]
