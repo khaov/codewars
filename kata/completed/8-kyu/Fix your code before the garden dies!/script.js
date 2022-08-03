f9354dd3aa7ab9358ecc2b6a86fe55b3946de0fe
Artyom Khaov <a.khaov@gmail.com>
2022-08-03 10:33:25 +0700

    Added: kata (8 kyu) Fix your code before the garden dies! instructions

--- kata/completed/8-kyu/Fix your code before the garden dies!/script.js
+++ kata/completed/8-kyu/Fix your code before the garden dies!/script.js
@@ -1,7 +1,8 @@
 // Solution:
 
 function rainAmount(mm){
-  return mm < 40 ? "You need to give your plant " + `${Math.abs(mm - 40)}` + "mm of water" : "Your plant has had more than enough water for today!";
+  return mm < 40 ? "You need to give your plant " + `${Math.abs(mm - 40)}` + "mm of water"
+                 : "Your plant has had more than enough water for today!";
 }
 
 // Sample Tests:
