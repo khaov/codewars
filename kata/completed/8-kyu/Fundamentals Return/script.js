5151b83a11a768213bf33dd74608e1740e87ed9f
Artyom Khaov <a.khaov@gmail.com>
2022-08-04 11:30:16 +0700

    Solved: completed kata (8 kyu) Fundamentals: Return

---
+++ kata/completed/8-kyu/Fundamentals Return/script.js
@@ -0,0 +1,34 @@
+// Solution:
+
+function add(a,b){
+  return a + b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function mod(a,b){
+  return a % b;
+}
+
+function exponent(a,b){
+  return Math.pow(a, b);
+}
+
+function subt(a,b){
+  return a - b;
+}
+
+// Sample Tests:
+
+add(1,2)      //  3
+multiply(1,2) //  2
+divide(2,1)   //  2
+mod(1,2)      //  1
+exponent(1,2) //  1
+subt(1,2)     //  -1
