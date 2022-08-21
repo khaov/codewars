9d9500f23bc683536b84e5299966102769df5caf
Artyom Khaov <a.khaov@gmail.com>
2022-08-21 22:31:34 +0700

    Solved: completed kata (8 kyu) Printing Array elements with Comma delimiters

---
+++ kata/completed/8-kyu/Printing Array elements with Comma delimiters/script.js
@@ -0,0 +1,10 @@
+// Solution:
+
+function printArray(array){
+  return array.join(',');
+}
+
+// Sample Tests:
+
+printArray(["h","o","l","a"]) // "h,o,l,a"
+printArray([2,4,5,2])         // "2,4,5,2"
