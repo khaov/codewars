e6e893b650784146c9b52e8bb80f2d00efe6e5e0
Artyom Khaov <a.khaov@gmail.com>
2022-08-21 16:17:09 +0700

    Solved: completed kata (8 kyu) Feynman's square question

---
+++ kata/completed/6-kyu/Feynman's square question/script.js
@@ -0,0 +1,18 @@
+// Solution:
+
+function countSquares(n) {
+  let squares = 0;
+
+  for (let i = 0; i <=n; i++) {
+    squares += Math.pow(n-i, 2);
+  }
+
+  return squares;
+}
+
+// Sample Tests:
+
+countSquares(1)   // 1
+countSquares(2)   // 5
+countSquares(5)   // 55
+countSquares(15)  // 1240
