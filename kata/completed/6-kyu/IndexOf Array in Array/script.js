8c54117f1f001b5b4dbd06ea661736b2389023d2
Artyom Khaov <a.khaov@gmail.com>
2022-09-02 13:02:36 +0700

    Added: kata (6 kyu) IndexOf Array in Array draft

---
+++ kata/completed/6-kyu/IndexOf Array in Array/script.js
@@ -0,0 +1,24 @@
+// Solution:
+
+var searchArray = function (arrayToSearch, query) {
+  // TODO your code here
+}
+
+// Sample Tests:
+
+// valid input
+
+var bigArray = [[2,3],[7,2],[9,20],[1,2],[7,2],[45,4],[7,87],[4,5],[2,7],[6,32]];
+
+var searchFor = [9,20];
+searchArray(bigArray,searchFor) // 2
+
+searchFor = [1,12];
+searchArray(bigArray,searchFor) // -1
+
+searchFor = [7,2];
+searchArray(bigArray, searchFor) // 1
+
+// throw Error
+
+
