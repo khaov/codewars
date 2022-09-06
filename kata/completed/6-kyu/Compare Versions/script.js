cba48087caed23a063340b65469e0628148d0592
Artyom Khaov <a.khaov@gmail.com>
2022-09-07 00:08:00 +0700

    Solved: completed kata (6 kyu) Compare Versions

--- kata/completed/6-kyu/Compare Versions/script.js
+++ kata/completed/6-kyu/Compare Versions/script.js
@@ -9,10 +9,18 @@ function compareVersions (version1, version2) {
   const versionLength = Math.max(versionName1.length, versionName2.length);
 
   for (let v = 0; v < versionLength; v++) {
-    if (Number(versionName1[v] || 0) < Number(versionName2[v] || 0)) {
+    if (!versionName1[v]) {versionName1.push(0)}
+    if (!versionName2[v]) {versionName2.push(0)}
+  }
+
+  versionName1.reverse();
+  versionName2.reverse();
+
+  for (let v = 0; v < versionLength; v++) {
+    if (Number(versionName1[v]) < Number(versionName2[v])) {
       compare = false;
-    } else {
-      compare = true; 
+    } else if (Number(versionName1[v]) > Number(versionName2[v])) {
+      compare = true;
     }
   }
 
