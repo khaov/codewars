da2544aaf61bef8cebf53493c05667c6cb3b42cb
Artyom Khaov <a.khaov@gmail.com>
2022-09-09 00:57:04 +0700

    Solved: completed kata (6 kyu) IndexOf Array in Array

--- kata/completed/6-kyu/IndexOf Array in Array/script.js
+++ kata/completed/6-kyu/IndexOf Array in Array/script.js
@@ -1,12 +1,19 @@
 // Solution:
 
 var searchArray = function (arrayToSearch, query) {
-  // TODO your code here
+  if (
+    arrayToSearch.every(item => Array.isArray(item) && item.length === 2) &&
+    (Array.isArray(query) && query.length === 2)
+  ) {
+    return arrayToSearch.map(item => item.toString()).indexOf(query.toString());
+  } else {
+    throw new Error();
+  }
 }
 
 // Sample Tests:
 
-// valid input
+// valid array & valid query
 
 var bigArray = [[2,3],[7,2],[9,20],[1,2],[7,2],[45,4],[7,87],[4,5],[2,7],[6,32]];
 
@@ -19,6 +26,24 @@ searchArray(bigArray,searchFor) // -1
 searchFor = [7,2];
 searchArray(bigArray, searchFor) // 1
 
-// throw Error
+// invalid array & valid query
 
+bigArray = [[1,2],[3,4],[5,6],[7,8,9]];
 
+searchFor = [1,2];
+searchArray(bigArray,searchFor) // throw Error
+
+bigArray = [1,2,3,4,5,6];
+
+searchFor = [1,2];
+searchArray(bigArray,searchFor) // throw Error
+
+// valid array & invalid query
+
+bigArray = [[1,2],[3,4],[5,6],[7,8]];
+
+searchFor = [9,2,1];
+searchArray(bigArray,searchFor) // throw Error
+
+searchFor = 5;
+searchArray(bigArray,searchFor) // throw Error
