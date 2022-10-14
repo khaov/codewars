34b16b0a0012aed1efde6c878cf643b70cd01020
Artyom Khaov <a.khaov@gmail.com>
2022-10-14 11:04:54 +0700

    Solved: completed kata (8 kyu) Grasshopper - Messi Goals

---
+++ kata/completed/8-kyu/Grasshopper - Messi Goals/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+// Sample Tests:
+
+totalGoals // 58
