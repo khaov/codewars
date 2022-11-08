385b7ff353f4f56f6b0f22f249dfa207eadacdc8
Artyom Khaov <a.khaov@gmail.com>
2022-11-08 10:50:53 +0700

    Solved: completed kata (8 kyu) Grasshopper - Debug sayHello

---
+++ kata/completed/8-kyu/Grasshopper - Debug sayHello/script.js
@@ -0,0 +1,12 @@
+// Solution:
+
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
+
+// Sample Tests:
+
+sayHello('Mr. Spock')       // 'Hello, Mr. Spock'
+sayHello('Captain Kirk')    // 'Hello, Captain Kirk'
+sayHello('Liutenant Uhura') // 'Hello, Liutenant Uhura'
+sayHello('Dr. McCoy')       // 'Hello, Dr. McCoy'
