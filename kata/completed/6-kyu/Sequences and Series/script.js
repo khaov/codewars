8c60108195fd9cb972da3dd52cc6c8424ed3354f
Artyom Khaov <a.khaov@gmail.com>
2022-11-28 09:52:46 +0700

    Solved: completed kata (6 kyu) Sequences and Series

---
+++ kata/completed/6-kyu/Sequences and Series/script.js
@@ -0,0 +1,13 @@
+// Solution:
+
+function getScore(n) {
+  return n * (n + 1) * 25;
+}
+
+// Sample Tests:
+
+getScore(1) // 50
+getScore(2) // 150
+getScore(3) // 300
+getScore(4) // 500
+getScore(5) // 750
