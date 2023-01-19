1707867147a5afdeb306c809d396edfe8951c7b4
Artyom Khaov <a.khaov@gmail.com>
2023-01-19 10:07:34 +0700

    Solved: completed kata (8 kyu) Grasshopper - Bug Squashing

---
+++ kata/completed/8-kyu/Grasshopper - Bug Squashing/script.js
@@ -0,0 +1,16 @@
+// Solution:
+
+var health = 100
+var position = 0
+var coins = 0
+
+function main () {
+  rollDice()
+  move()
+  combat()
+  getCoins()
+  buyHealth()
+  printStatus()
+}
+
+// Sample Tests:
