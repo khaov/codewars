20aa32ca56664c51ce5f14162a2fdc3668f547ae
Artyom Khaov <a.khaov@gmail.com>
2023-02-27 09:51:30 +0700

    Solved: completed kata (7 kyu) Spacify

---
+++ kata/completed/7-kyu/Spacify/script.js
@@ -0,0 +1,10 @@
+// Solution:
+
+function spacify(str) {
+  return str.split('').join(' ')
+}
+
+// Sample Tests:
+
+spacify('hello world') // 'h e l l o   w o r l d'
+spacify('12345') // '1 2 3 4 5'
