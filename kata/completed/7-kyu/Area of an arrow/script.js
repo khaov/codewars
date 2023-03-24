0df9509b536ad09fe192b2e1688bf2c562b17f7f
Artyom Khaov <a.khaov@gmail.com>
2023-03-24 15:38:10 +0700

    Solved: completed kata (7 kyu) Area of an arrow

---
+++ kata/completed/7-kyu/Area of an arrow/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+function arrowArea(a,b) {
+    return (a * b / 4).toFixed(2) * 1
+}
+
+// Sample Tests:
+
+arrowArea(4,2)      // 2
+arrowArea(7,6)      // 10.5
+arrowArea(25,25)    // 156.25
