1ac0ea9d9f1ecc6c4559a74946aa085f5b34f45f
Artyom Khaov <a.khaov@gmail.com>
2023-11-12 01:20:36 +0700

    Solved: completed kata (8 kyu) Grasshopper - Create the rooms

---
+++ kata/completed/8-kyu/Grasshopper - Create the rooms/script.js
@@ -0,0 +1,21 @@
+// Solution:
+
+var rooms = {
+  room1: {
+    name: "name1",
+    description: "description1",
+    completed: "completed1"
+  },
+  room2: {
+    name: "name2",
+    description: "description2",
+    completed: "completed2"
+  },
+  room3: {
+    name: "name3",
+    description: "description3",
+    completed: "completed3"
+  }
+}
+
+// Sample Tests:
