0c85677b35c3bb50b7d3da94b91f4ac8d70ac26e
Artyom Khaov <a.khaov@gmail.com>
2023-12-13 10:53:58 +0700

    Solved: completed kata (8 kyu) Incorrect division method

---
+++ kata/completed/8-kyu/5 without numbers !! copy/script.js
@@ -0,0 +1,13 @@
+// Solution:
+
+const solve = (x, y) => x / y
+
+// Sample Tests:
+
+solve(2, 1)     // 2
+solve(2, 4)     // 0.5
+solve(42, 0)    // Infinity
+solve(0, 8)     // 0
+solve(7, 2)     // 3.5
+solve(4.2, 1)   // 4.2
+solve(9999, 11) // 909
