4fdfa36f6a5c81780a4d34972b8b1c5a79ddb706
Artyom Khaov <a.khaov@gmail.com>
2023-12-27 17:07:07 +0700

    Solved: completed kata (8 kyu) easy logs

---
+++ kata/completed/8-kyu/easy logs/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+function logs(x, a, b) {
+  return (Math.log(a * b) / Math.log(x));
+}
+
+// Sample Tests:
+
+logs(10, 2, 3)    // 0.7781512503836435
+logs(5, 2, 3)     // 1.1132827525593785
+logs(1000, 2, 3)  // 0.25938375012788123
