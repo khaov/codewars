c85ba650cd6a9bb8b27f5d3b5e5c3c76b1df57ad
Artyom Khaov <a.khaov@gmail.com>
2023-12-31 21:47:03 +0700

    Solved: completed kata (7 kyu) Perimeter sequence

---
+++ kata/completed/7-kyu/Perimeter sequence/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+function perimeterSequence(a,n) {
+  return a * n * 4;
+}
+
+// Sample Tests:
+
+perimeterSequence(1,1) //  4
+perimeterSequence(1,2) //  8
+perimeterSequence(1,3) //  12
