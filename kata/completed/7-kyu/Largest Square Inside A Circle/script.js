f1e568226b2646c98c6b57b97a357b1b974fdf13
Artyom Khaov <a.khaov@gmail.com>
2024-01-02 00:52:47 +0700

    Solved: completed kata (7 kyu) Largest Square Inside A Circle

---
+++ kata/completed/7-kyu/Largest Square Inside A Circle/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+function areaLargestSquare(r) {
+  return Math.pow(2 * r, 2) / 2;
+}
+
+// Sample Tests:
+
+areaLargestSquare(5)  // 50
+areaLargestSquare(7)  // 98
+areaLargestSquare(15) // 450
