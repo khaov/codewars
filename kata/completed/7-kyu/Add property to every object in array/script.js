a23e4c5a8764fd62f288a6902453fe4a7132f246
Artyom Khaov <a.khaov@gmail.com>
2024-01-04 02:18:50 +0700

    Solved: completed kata (7 kyu) Add property to every object in array

---
+++ kata/completed/7-kyu/Add property to every object in array/script.js
@@ -0,0 +1,17 @@
+// Solution:
+
+var questions = [{
+  question: "What's the currency of the USA?",
+  choices: ["US dollar", "Ruble", "Horses", "Gold"],
+  corAnswer: 0
+}, {
+  question: "Where was the American Declaration of Independence signed?",
+  choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+  corAnswer: 0
+}];
+
+questions.forEach(question => question.usersAnswer = null);
+
+// Sample Tests:
+
+questions
