d9b7811bbdfd5594a5c6f81b35dc6032eb6a453f
Artyom Khaov <a.khaov@gmail.com>
2024-09-30 15:03:11 +0700

    Solved: completed kata (8 kyu) Template Strings

---
+++ kata/completed/8-kyu/Template Strings/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+var templateStrings = function(noun, adjective) {
+  return `${noun} are ${adjective}`;
+}
+
+// Sample Tests:
+
+templateStrings('Animals', 'Good'); // 'Animals are Good'
+templateStrings('You', 'special');  // 'You are special'
+templateStrings('lives', 'frozen'); // 'lives are frozen'
