015c57cbbfa9d45920f51a8d65a115545ee64ccf
Artyom Khaov <a.khaov@gmail.com>
2024-10-06 22:54:38 +0700

    Solved: completed kata (7 kyu) Nth Root of a Number

---
+++ kata/completed/7-kyu/Nth Root of a Number/script.js
@@ -0,0 +1,13 @@
+// Solution:
+
+function root(x, n) {
+  return Math.pow(x, 1 / n);
+}
+
+// Sample Tests:
+
+root(4, 2)    // 2
+root(8, 3)    // 2
+root(256, 4)  // 4
+root(9, 2)    // 3
+root(6.25, 2) // 2.5
