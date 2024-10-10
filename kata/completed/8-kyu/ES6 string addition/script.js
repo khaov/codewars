8d8ba6f2e6ae485aaee97d863b6a24a35c9fe1b8
Artyom Khaov <a.khaov@gmail.com>
2024-10-10 23:17:59 +0700

    Solved: completed kata (8 kyu) ES6 string addition

---
+++ kata/completed/8-kyu/ES6 string addition/script.js
@@ -0,0 +1,11 @@
+// Solution:
+
+function joinStrings(string1, string2) {
+  return `${string1} ${string2}`
+}
+
+// Sample Tests:
+
+joinStrings('string1', 'string2') // 'string1 string2'
+joinStrings('testing', 'testing') // 'testing testing'
+joinStrings(134, 234)             // '134 234'
